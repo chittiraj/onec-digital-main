5f4e371714ded4028dd94e8dda684d80b8c99fdc
chittiraj <chittirajaachari.k@gmail.com>
2026-08-05 20:29:55 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import "./styles/index.css";
 if ("scrollRestoration" in window.history) {
     window.history.scrollRestoration = "manual";
 }
+
 createRoot(document.getElementById("root")).render(
 
     <StrictMode>
